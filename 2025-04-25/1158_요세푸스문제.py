e4338f6f8ef160f5e2459d19eea552bec6268d9b
ggzzggzzz <ziobang77@hs.ac.kr>
2025-04-26 17:51:17 +0900

    풀다가 그만둠

--- 2025-04-25/1158_요세푸스문제.py
+++ 2025-04-25/1158_요세푸스문제.py
@@ -1,9 +1,15 @@
 N, k = map(int,input().split())
-
 arr = [i + 1 for i in range(N)]
-print(arr)
+result = []
+a = 0
 
-#for i in range(N):
+for i in range(N):
+    a = (a + k - 1) % len(arr)
+    result.append(arr.pop(a))
+
+print(result)
+
+
 
 
 
