8438cc00764fefacf5fe35d091ac84df2a2538ec
ggzzggzzz <ziobang77@hs.ac.kr>
2025-04-27 17:20:38 +0900

    .

---
+++ 2025-04-27/10870_피보나치5.py
@@ -0,0 +1,11 @@
+def fibo(a):
+    if a == 0:
+        return 0
+    elif a == 1:
+        return 1
+    else:
+        b = fibo(a-1) + fibo(a-2)
+        return b
+
+n = int(input())
+print(fibo(n))
