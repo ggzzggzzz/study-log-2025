330dd22df6b52d8db1dac7377be620b680c47ff3
ggzzggzzz <162405948+ggzzggzzz@users.noreply.github.com>
2025-04-27 23:04:30 +0900

    Update 25501_재귀의귀재.py
    파이썬 함수 반환값 2개 되는거 처음알았음

--- 2025-04-27/25501_재귀의귀재.py
+++ 2025-04-27/25501_재귀의귀재.py
@@ -1,1 +1,17 @@
+def recursion(s, l, r, cnt):
+    cnt += 1
+    if l >= r: return 1,cnt
+    elif s[l] != s[r]: return 0, cnt
+    else: return recursion(s, l+1, r-1,cnt)
+
+def isPalindrome(s):
+    cnt = 0
+    return recursion(s, 0, len(s)-1, cnt)
+T = int(input())
+s = list(input() for i in range(T))
+
+for i in range(len(s)):
+    a , b = isPalindrome(s[i])
+    print(a,b)
+
 
